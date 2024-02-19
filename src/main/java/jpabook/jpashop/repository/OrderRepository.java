5066220ff69b6af13d45ce50a2e58829bcbd35fd
SUMIN <lovepoemlovepoem0516@gmail.com>
2024-02-20 01:50:22 +0900

    feat: 주문 도메인 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    // 주문 저장
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    // 주문 조회
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+/*
+    // 검색
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+    }
+*/
+
+}
