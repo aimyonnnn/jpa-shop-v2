8e29dfbe02580df6fbf7d06efb8da8a689bd4f0b
SUMIN <123183535+aimyonnnn@users.noreply.github.com>
2024-02-21 16:32:32 +0900

    comment: 주석 수정

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+
+
+
+
+
+
+}
